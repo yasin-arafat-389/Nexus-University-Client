0d92a3306cee80e6bc4c0a697c5005f92e9f32d9
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-07-27 11:06:40 +0600

    Created a reusable form input component

---
+++ src/Forms/FormInput.tsx
@@ -0,0 +1,25 @@
+import { Controller } from "react-hook-form";
+import { Form, Input } from "antd";
+
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
+
+const FormInput = ({ type, name, label }: TInputProps) => {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      <Controller
+        name={name}
+        render={({ field }) => (
+          <Form.Item label={label}>
+            <Input {...field} type={type} id={name} size="large" />
+          </Form.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+export default FormInput;
