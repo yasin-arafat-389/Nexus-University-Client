b954beb650553f2a61830883a9271b976d700d89
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-01 10:21:14 +0600

    Created api layer for academic managements

---
+++ src/Redux/Features/Admin/academicManagement.api.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TQueryParam, TResponseRedux } from "../../../Types";
+import { TAcademicSemester } from "../../../Types/academicManagement.types";
+import { baseApi } from "../../API/baseAPI";
+
+const academicManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllSemesters: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/academic-semester",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicSemester: builder.mutation({
+      query: (data) => ({
+        url: "/academic-semester/create-academic-semester",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllSemestersQuery, useAddAcademicSemesterMutation }: any =
+  academicManagementApi;
