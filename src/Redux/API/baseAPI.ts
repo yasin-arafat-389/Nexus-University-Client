4200b2a63eb2551079277d990ff0523f028e9356
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-02 11:00:35 +0600

    Created base api layer

---
+++ src/Redux/API/baseAPI.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout, setUser } from "../Features/Auth/authSlice";
+import { toast } from "sonner";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.data === "You are unauthorized") {
+    //* Send Refresh Token
+    console.log("Sending refresh token");
+
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+
+      if (result?.error) {
+        toast.error("Something went wrong!!");
+      }
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  endpoints: () => ({}),
+});
