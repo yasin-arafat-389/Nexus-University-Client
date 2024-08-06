cf2b0aba8cbc39d7f60ad68aabb3dd063289da5a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-06 09:35:08 +0600

    Created student routes

---
+++ src/Routes/Student.routes.tsx
@@ -0,0 +1,15 @@
+import OfferedCourse from "../Pages/Student/OfferedCourse";
+import StudentDashboard from "../Pages/Student/StudentDashboard";
+
+export const studentPaths = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: <StudentDashboard />,
+  },
+  {
+    name: "Offered Course",
+    path: "offered-course",
+    element: <OfferedCourse />,
+  },
+];
