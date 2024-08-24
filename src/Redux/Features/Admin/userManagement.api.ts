d1fdbb57c78f053d53b19dbeaf0fcc44e05c1194
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-24 08:53:41 +0600

    Added some necessary endpoints in user management api layer

---
+++ src/Redux/Features/Admin/userManagement.api.ts
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TQueryParam, TResponseRedux, TStudent } from "../../../Types";
+import { baseApi } from "../../API/baseAPI";
+
+const userManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllStudents: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/students",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TStudent[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+
+    addStudent: builder.mutation({
+      query: (data) => ({
+        url: "/user/create-student",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
+    getAllFaculties: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/faculties",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TStudent[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+
+    changePassword: builder.mutation({
+      query: (data) => ({
+        url: "/auth/change-password",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useAddStudentMutation,
+  useGetAllStudentsQuery,
+  useGetAllFacultiesQuery,
+  useChangePasswordMutation,
+}: any = userManagementApi;
