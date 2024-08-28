0e0293e1ae30cbf7e2665821a64d440a0b0653ca
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-08-28 11:18:41 +0600

    Created api layer for student course management

---
+++ src/Redux/Features/Student/studentCourseManagement.api.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TQueryParam, TResponseRedux } from "../../../Types";
+import { TOfferedCourse } from "../../../Types/studentCourse.type";
+import { baseApi } from "../../API/baseAPI";
+
+const studentCourseApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllOfferedCourses: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/offered-courses/my-offered-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["offeredCourse"],
+      transformResponse: (response: TResponseRedux<TOfferedCourse[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    getAllEnrolledCourses: builder.query({
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses/my-enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["offeredCourse"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    enrolCourse: builder.mutation({
+      query: (data) => ({
+        url: "/enrolled-courses/create-enrolled-course",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["offeredCourse"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllOfferedCoursesQuery,
+  useEnrolCourseMutation,
+  useGetAllEnrolledCoursesQuery,
+}: any = studentCourseApi;
