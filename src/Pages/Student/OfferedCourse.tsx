3150bfedb72ce3954c83685153f8840d50934127
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-09-01 09:44:03 +0600

    Designed offered course page

---
+++ src/Pages/Student/OfferedCourse.tsx
@@ -0,0 +1,86 @@
+import { Button, Col, Row } from "antd";
+import {
+  useEnrolCourseMutation,
+  useGetAllOfferedCoursesQuery,
+} from "../../Redux/Features/Student/studentCourseManagement.api";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type TCourse = {
+  [index: string]: any;
+};
+
+const OfferedCourse = () => {
+  const { data: offeredCourseData } = useGetAllOfferedCoursesQuery(undefined);
+  const [enroll] = useEnrolCourseMutation();
+
+  const singleObject = offeredCourseData?.data?.reduce(
+    (acc: TCourse, item: any) => {
+      const key = item.course.title;
+      acc[key] = acc[key] || { courseTitle: key, sections: [] };
+      acc[key].sections.push({
+        section: item.section,
+        _id: item._id,
+        days: item.days,
+        startTime: item.startTime,
+        endTime: item.endTime,
+      });
+      return acc;
+    },
+    {}
+  );
+
+  const modifiedData = Object.values(singleObject ? singleObject : {});
+
+  const handleEnroll = async (id: any) => {
+    const enrollData = {
+      offeredCourse: id,
+    };
+
+    const res = await enroll(enrollData);
+    console.log(res);
+  };
+
+  if (!modifiedData.length) {
+    return <p>No available courses</p>;
+  }
+
+  return (
+    <Row gutter={[0, 20]}>
+      {modifiedData.map((item: any) => {
+        return (
+          <Col span={24} style={{ border: "solid #d4d4d4 2px" }}>
+            <div style={{ padding: "10px" }}>
+              <h2>{item.courseTitle}</h2>
+            </div>
+            <div>
+              {item.sections.map((section: any) => {
+                return (
+                  <Row
+                    justify="space-between"
+                    align="middle"
+                    style={{ borderTop: "solid #d4d4d4 2px", padding: "10px" }}
+                  >
+                    <Col span={5}>Section: {section.section} </Col>
+                    <Col span={5}>
+                      days:{" "}
+                      {section.days.map((day: any) => (
+                        <span> {day} </span>
+                      ))}
+                    </Col>
+                    <Col span={5}>Start Time: {section.startTime} </Col>
+                    <Col span={5}>End Time: {section.endTime} </Col>
+                    <Button onClick={() => handleEnroll(section._id)}>
+                      Enroll
+                    </Button>
+                  </Row>
+                );
+              })}
+            </div>
+          </Col>
+        );
+      })}
+    </Row>
+  );
+};
+
+export default OfferedCourse;
